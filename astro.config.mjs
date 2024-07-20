ce7ba22835e77749821a28a213f31d0a4047f541
mspiegel31 <mspiegel31@users.noreply.github.com>
2024-07-20 14:08:46 -0400

    chore: Add ci (#1)
    * stub CI
    
    * update config
    
    * fix name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mspiegel31.github.io/',
+	basePath: 'mike-spiegel',
 	integrations: [mdx(), sitemap()],
 });
